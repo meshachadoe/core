33dc39a004ee7f96033722ab9b848d5056968870
meshachadoe <marlienri.avehas@gmail.com>
2020-08-21 17:09:36 +0700

    Add offset to scroll and add favicon

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,50 @@
+// window.addEventListener("hashchange", function () {
+//     console.log("here");
+//     window.scrollTo(window.scrollX, window.scrollY);
+// });
+
+// function smoothScroll(target, duration) {
+//     var target = document.querySelector(target);
+//     var targetPosition = target.getBoundingClientRect().top;
+//     // Relative to the window of the actual element
+//     var startPosition = window.pageYOffset;
+//     var distance = targetPosition - startPosition;
+//     var startTime = null;
+
+//     function animation(currentTime) {
+//         if(startTime === null) startTime = currentTime;
+//         var timeElapsed = currentTime - startTime;
+//         var run = ease(timeElapsed, startPosition, distance, duration);
+//         window.scrollTo(0,run);
+//         if(timeElapsed < duration) requestAnimationFrame(animation);
+//     }
+
+//     function ease(t, b, c, d) {
+//         t /= d / 2;
+//         if(t < 1) return c / 2 * t * t + b;
+//         t--;
+//         return -c / 2 * (t * (t - 2) - 1) + b;
+//     }
+
+//     requestAnimationFrame(animation);
+// }
+
+// // var about = document.querySelector('.nav-about');
+// about.addEventListener('click', function() {
+//     console.log("print");
+//     smoothScroll('.section-program-1', 1000);
+// })
+
 // Only run this when the document is ready
 $(document).ready(function() {
 
+    $('.scrollTop').click(function() {
+        $('<a name="top"/>').insertBefore($('body').children().eq(0));
+        window.location.hash = 'top';
+    });
+
     /* Animations on scroll */
+
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated animate__animated animate__fadeIn');
     }, {
